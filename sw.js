8ad1a929bf4dae6a3e549c41aeb087796aaac615
yymeantheworldtokk <kunwei39@gmail.com>
2024-07-19 23:52:48 +0800

    Site updated: 2024-07-19 23:52:48

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1720445052919";
+const VERSION = "1721404356071";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
